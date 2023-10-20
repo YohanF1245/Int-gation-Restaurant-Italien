1526f46ba1dc46e17b8110ba390413bbdaad6580
YohanF1245 <148520972+YohanF1245@users.noreply.github.com>
2023-10-20 09:55:36 +0200

    update20/10/2023

---
+++ script.js
@@ -0,0 +1,4 @@
+function parseSearch(){
+    let params = 'scrollbars=no,resizable=no,status=no,location=yes,toolbar=no,menubar=no,width=500,height=200';
+    window.open("popup.html",'_blank',params);
+}
