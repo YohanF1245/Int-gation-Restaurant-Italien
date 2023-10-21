ecd192845b3e5016078ad63c3baabf45e9d63efd
YohanF1245 <148520972+YohanF1245@users.noreply.github.com>
2023-10-21 23:37:29 +0200

    update21/03/2023
    premiere version complétée

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+var searchString;
 function parseSearch(){
+    // searchString= document.getElementById("searchBar").value;
+    sessionStorage.setItem("searchString", document.getElementById("searchBar").value);
     let params = 'scrollbars=no,resizable=no,status=no,location=yes,toolbar=no,menubar=no,width=500,height=200';
     window.open("popup.html",'_blank',params);
+    
+}
+function fillSearch(){
+    document.getElementById("searchingText").innerHTML =("Recherche de "+sessionStorage.getItem("searchString")+" en cours ...");
 }
