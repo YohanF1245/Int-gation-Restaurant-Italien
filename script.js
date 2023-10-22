5654b7d9c9298aded6b142ed254bda83c6e421d4
YohanF1245 <148520972+YohanF1245@users.noreply.github.com>
2023-10-22 19:21:04 +0200

    update22-10-23

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ function parseSearch(){
 function fillSearch(){
     document.getElementById("searchingText").innerHTML =("Recherche de "+sessionStorage.getItem("searchString")+" en cours ...");
 }
+function goToS1(){
+    window.location.href = "#s1";
+}
+function goToS2(){
+    window.location.href = "#s2";
+}
+function goToS3(){
+    window.location.href = "#s3";
+}
+function goToS4(){
+    window.location.href = "#s4";
+}
+function goToS5(){
+    window.location.href = "#s5";
+}
+function goToS6(){
+    window.location.href = "#s6";
+}
+function goToS7(){
+    window.location.href = "#s7";
+}
+function goToS8(){
+    window.location.href = "#s8";
+}
+// intersectionhandler
+var observer = new IntersectionObserver(function(entries) {
+	// isIntersecting is true when element and viewport are overlapping
+	// isIntersecting is false when element and viewport don't overlap
+	if(entries[0].isIntersecting === true)
+		document.getElementById("s2Radio").checked = true;
+}, { threshold: [0] });
+
+observer.observe(document.querySelector("#s2"));
