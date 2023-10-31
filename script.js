7fd64980a1d26af5b6e1230dc7d66600d4f2ac5b
YohanF1245 <148520972+YohanF1245@users.noreply.github.com>
2023-10-31 15:18:33 +0100

    31-10-23

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ function parseSearch(){
     sessionStorage.setItem("searchString", document.getElementById("searchBar").value);
     let params = 'scrollbars=no,resizable=no,status=no,location=yes,toolbar=no,menubar=no,width=500,height=200';
     window.open("popup.html",'_blank',params);
-    
 }
 function fillSearch(){
     document.getElementById("searchingText").innerHTML =("Recherche de "+sessionStorage.getItem("searchString")+" en cours ...");
-    
     let checked = document.getElementById("s2Radio").checked = true;
     console.log(checked)
-
-
 }
 function goToS1(){
     window.location.href = "#s1";
